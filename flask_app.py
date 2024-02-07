f18a0ea3b0a48f3f01d7281547fa6f03d1e034f5
Garnet <matolagarnet@gmail.com>
2024-02-07 05:38:50 -0800

    Added website layout for front-end

---
+++ flask_app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/anime.html')
+def anime():
+    anime_id = request.args.get('anime_id')
+
+    return render_template('anime.html', anime_id=anime_id)
