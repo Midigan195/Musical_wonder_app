07fd212fb73bea6f93a7443a9b98e1fff5ef63f7
Garnet Matola <matolagarnet.com>
2024-02-21 00:07:01 -0800

    added template documentation

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-
+""" This application opens both web pages for the site """
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def open_homepage():
+    """ This route returns the homepage """
     return render_template('index.html')
 
 @app.route('/anime.html')
 def anime():
+    """ This returns the anime page """
     anime_id = request.args.get('anime_id')
 
     return render_template('anime.html', anime_id=anime_id)
